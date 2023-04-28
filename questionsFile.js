e407c8fcb5172628fee82d2481c8e1bd28b12fb7
jairo ramirez <119916788+JayR86@users.noreply.github.com>
2023-04-27 19:22:18 -0700

    Add files via upload
    this are the new files without plagiarism

--- questionsFile.js
+++ questionsFile.js
@@ -1,102 +1,71 @@
-var timeElement = document.querySelector("#time");
-var wrapperElement = document.querySelector(".wrapper");
-var btnElement = document.querySelector("#start");
-var divContEL = document.querySelector(".divContainer");
-var hElement = document.querySelector("#title");
-var oderListEl = document.querySelector("#q-list");
-var finishDiv = document.querySelector(".finish-section");
-var finalScore = document.querySelector("#result");
-var errMsg = document.querySelector("#errorSmg");
-var initialInput = document.querySelector("#inputInitial").value;
-var submitEl = document.querySelector(".btn btn-primary mb-2");
-var responsDiv = document.querySelector("#response");
-var finaPageEl = document.querySelector(".final-page");
-var initialAndScore = document.querySelector("#staticEmail");
-var firstPageEl = document.querySelector(".first-page");
+// Get DOM elements
+const timeElement = document.querySelector("#time");
+const wrapperElement = document.querySelector(".wrapper");
+const btnElement = document.querySelector("#start");
+const divContEL = document.querySelector(".divContainer");
+const hElement = document.querySelector("#title");
+const oderListEl = document.querySelector("#q-list");
+const finishDiv = document.querySelector(".finish-section");
+const finalScore = document.querySelector("#result");
+const errMsg = document.querySelector("#errorSmg");
+const initialInput = document.querySelector("#inputInitial");
+const submitEl = document.querySelector(".btn.btn-primary.mb-2");
+const responsDiv = document.querySelector("#response");
+const finaPageEl = document.querySelector(".final-page");
+const initialAndScore = document.querySelector("#staticEmail");
+const firstPageEl = document.querySelector(".first-page");
 
+// Create an array of questions
+const questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title:
+      "There are 3 different ways in which a JavaScript code can be involved in an HTML file. Selct the one that's not correct.",
+    choices: ["Inline", "Import", "External", "Internal"],
+    answer: "Import",
+  },
+  {
+    title: "How to create an array in js ?",
+    choices: ["var A[]=", "var A{}=", "var A=[]", "var A={}"],
+    answer: "var A=[]",
+  },
+  {
+    title:
+      "HTML element that can be accessed in a Javascript code: Chose the one that will return an array of elements",
+    choices: [
+      "getElementById(‘idname’)",
+      "getElementsByClass(‘classname’)",
+      "getElementsByTagName(‘tagname’)",
+      "querySelectorAll()",
+    ],
+    answer: "querySelectorAll()",
+  },
+];
 
-
-// Create an  array of questions
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts",
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses",
-    },
-    {
-        title: "There are 3 different ways in which a JavaScript code can be involved in an HTML file. Selct the one that's not correct.",
-        choices: ["Inline", "Import", "External", "Internal"],
-        answer: "Import",
-    },
-    {
-        title: "How to create an array in js ?",
-        choices: ["var A[]=", "var A{}=", "var A=[]", "var A={}"],
-
-        answer: "var A=[]",
-    },
-    {   
-        title: "HTML element that can be accessed in a Javascript code: Chose the one that will return an array of elements",
-        choices: ["getElementById(‘idname’)", "getElementsByClass(‘classname’)", 
-        "getElementsByTagName(‘tagname’)", "querySelectorAll()"],
-        answer: "querySelectorAll()",
-    }
-]
-
-
- /**Create next questions to be added to the HTML document dynamically*/
+// Create next questions to be added to the HTML document dynamically
 function displayQuestions() {
-    var holdQ1Title = questions[i].title
-    hElement.textContent = holdQ1Title
-    var holdq1Choice1 = questions[i].choices[0];
-    var holdq1Choice2 = questions[i].choices[1];
-    var holdq1Choice3 = questions[i].choices[2];
-    var holdq1Choice4 = questions[i].choices[3];
-
-    oderListEl.innerHTML = '';
-
-    var liTag1 = document.createElement("li");
-    liTag1.setAttribute("class", "all_li")
-    var btn = document.createElement('button');
-    btn.setAttribute("class", "all_btn")
-    btn.textContent = holdq1Choice1;
-    liTag1.appendChild(btn)
-    oderListEl.appendChild(liTag1);
+  const question = questions[i];
+  const { title, choices } = question;
+  hElement.textContent = title;
+  oderListEl.innerHTML = "";
+  choices.forEach((choice) => {
+    const liTag = document.createElement("li");
+    liTag.setAttribute("class", "all_li");
+    const btn = document.createElement("button");
+    btn.setAttribute("class", "all_btn");
+    btn.textContent = choice;
+    liTag.appendChild(btn);
+    oderListEl.appendChild(liTag);
     divContEL.appendChild(oderListEl);
-
-    var liTag2 = document.createElement("li");
-    liTag2.setAttribute("class", "all_li");
-    var btn2 = document.createElement('button');
-    btn2.setAttribute("class", "all_btn")
-    btn2.textContent = holdq1Choice2;
-    liTag2.appendChild(btn2)
-    oderListEl.appendChild(liTag2)
-    divContEL.appendChild(oderListEl);
-
-    var liTag3 = document.createElement("li");
-    liTag3.setAttribute("class", "all_li")
-    var btn3 = document.createElement('button');
-    btn3.setAttribute("class", "all_btn")
-    btn3.textContent = holdq1Choice3;
-    liTag3.appendChild(btn3)
-    oderListEl.appendChild(liTag3)
-    divContEL.appendChild(oderListEl);
-
-    var liTag4 = document.createElement("li");
-    liTag4.setAttribute("class", "all_li")
-    var btn4 = document.createElement('button');
-    btn4.setAttribute("class", "all_btn");
-    btn4.textContent = holdq1Choice4;
-    liTag4.appendChild(btn4);
-    oderListEl.appendChild(liTag4);
-    divContEL.appendChild(oderListEl);
-    var allBtnEl = document.querySelectorAll(".all_btn")
-    allBtnEl.forEach(function (event) {
-        event.addEventListener("click", onclickHandler)
-    });
-
+    btn.addEventListener("click", onclickHandler);
+  });
 }
